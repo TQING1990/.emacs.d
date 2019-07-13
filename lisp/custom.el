717ab295a265ed4234569fc50fc49222ea9514f4
tqing <tangqing@onemt.com.cn>
2019-07-13 14:41:26 +0800

    添加git-gutter package，在buffer中显示修改

--- lisp/custom.el
+++ lisp/custom.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (counsel-etags company magit ivy smartparens lua-mode exec-path-from-shell flycheck yasnippet evil evil-leader evil-surround evil-nerd-commenter monokai-theme))))
+    (git-gutter counsel-etags company magit ivy smartparens lua-mode exec-path-from-shell flycheck yasnippet evil evil-leader evil-surround evil-nerd-commenter monokai-theme))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
