ed5515858030c773dfa41dc64f16e320c008548d
tqing <tangqing@onemt.com.cn>
2019-07-20 14:30:56 +0800

    这几个快捷键已经绑定到evil模式的快捷键中了

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -10,9 +10,7 @@
 ;;(setq enable-recursive-minibuffers t)
 (global-set-key (kbd "C-s") 'swiper)
 (global-set-key (kbd "M-x") 'counsel-M-x)
-;; (global-set-key (kbd "C-x C-f") 'counsel-git)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
 
 (provide 'init-ivy)
