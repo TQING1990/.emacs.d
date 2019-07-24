83ce87d130f192e631e5feb40116b1fa350570e9
tqing <tangqing@onemt.com.cn>
2019-07-24 09:13:22 +0800

    添加打开 magit 的快捷键

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -2,6 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 
+(global-set-key (kbd "C-x g") 'magit-status)
+
 ;; NOTE: run (global-git-gutter-mode t) AFTER (global-display-line-numbers-mode t)
 ;; OR  the left margin of the window will increase for whatever keystroke pressed.
 (global-git-gutter-mode t)
